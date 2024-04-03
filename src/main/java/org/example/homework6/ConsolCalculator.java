92cd926e03be7b9803b91d2c6f8e94e7576b363f
Irina Zakutilina <irinazakutilinaa>
2024-04-03 20:51:37 +0300

    Homework6: ConsolCalculator

---
+++ src/main/java/org/example/homework6/ConsolCalculator.java
@@ -0,0 +1,50 @@
+package org.example.homework6;
+import java.util.Scanner;
+public class ConsolCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String choice = "yes";
+
+        while (choice.equals("yes")) {
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter an arithmetic operation (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            double result;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 == 0) {
+                        System.out.println("Error: Division by zero");
+                        continue;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("Invalid operation");
+                    continue;
+            }
+
+            System.out.println("Result: " + result);
+
+            System.out.print("Do you want to perform another calculation? (yes/no): ");
+            choice = scanner.next().toLowerCase();
+        }
+
+        System.out.println("Exiting the calculator");
+        scanner.close();
+    }
+}
